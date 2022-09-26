41144e753e8acb0da54ce63b2e4efa88ea3ced91
Adriana Bertrand <abertran@c3r8s6.42madrid.com>
2022-09-26 18:21:02 +0200

    segundo

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,27 +1,38 @@
+/* ************************************************************************** */
+/*	                                                                        */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abertran <abertran@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 18:02:54 by abertran          #+#    #+#             */
+/*   Updated: 2022/09/26 18:04:12 by abertran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int neg;
-    int res;
-    
-    i = 0;
-    neg = 1;
-    res = 0;
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == ' ' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            neg *= -1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        res =  (str[i] - '0') + (res * 10);
-        i++;
-    }
-    return (res * neg);
-} 
+	int	i;
+	int	neg;
+	int	res;
+
+	i = 0;
+	neg = 1;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == ' ' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - '0') + (res * 10);
+		i++;
+	}
+	return (res * neg);
+}
--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,16 +6,16 @@
 /*   By: abertran <abertran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 15:58:33 by abertran          #+#    #+#             */
-/*   Updated: 2022/09/26 15:58:37 by abertran         ###   ########.fr       */
+/*   Updated: 2022/09/26 18:01:10 by abertran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
-    (c >= '0' && c <= '9'))
-        return (1);
-    return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
 }
